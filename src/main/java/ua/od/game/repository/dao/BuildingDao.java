a9c07a34a6a90ba878da437095194fa706db3985
root <root@roman.home>
2018-11-08 20:11:19 +0200

    Buildings Dao

--- src/main/java/ua/od/game/repository/dao/BuildingDao.java
+++ src/main/java/ua/od/game/repository/dao/BuildingDao.java
@@ -1,11 +1,11 @@
 package ua.od.game.repository.dao;
 
-import ua.od.game.model.BuildingsEntity;
-import ua.od.game.model.ResourcesEntity;
+import ua.od.game.model.BuildingEntity;
+import ua.od.game.model.ResourceEntity;
 
 import java.util.List;
 import java.util.Map;
 
 public interface BuildingDao {
-    Map<BuildingsEntity, ResourcesEntity> getAllBuildingList();
+    Map<BuildingEntity, List<ResourceEntity>> getAllBuildingList();
 }
--- src/main/java/ua/od/game/repository/dao/impl/BuildingDaoImpl.java
+++ src/main/java/ua/od/game/repository/dao/impl/BuildingDaoImpl.java
@@ -1,7 +1,7 @@
 package ua.od.game.repository.dao.impl;
 
-import ua.od.game.model.BuildingsEntity;
-import ua.od.game.model.ResourcesEntity;
+import ua.od.game.model.BuildingEntity;
+import ua.od.game.model.ResourceEntity;
 import ua.od.game.repository.dao.BuildingDao;
 import ua.od.game.repository.helper.SqlHelper;
 
@@ -15,29 +15,32 @@ import java.util.Map;
 public class BuildingDaoImpl implements BuildingDao {
 
     @Override
-    public Map<BuildingsEntity,ResourcesEntity> getAllBuildingList() {
+    public Map<BuildingEntity,List<ResourceEntity>> getAllBuildingList() {
         return SqlHelper.prepareStatement("SELECT  a.id, a.name, a.description, b.resource_id, b.number_per_sec " +
                 "from Building a inner join Building_Product b on a.id = b.building_id", pstmt -> {
             ResultSet rs = pstmt.executeQuery();
-            Map<BuildingsEntity,ResourcesEntity> map = new LinkedHashMap<>();
-            List<ResourcesEntity> list = new LinkedList<>();
+            Map<BuildingEntity,List<ResourceEntity>> map = new LinkedHashMap<>();
+            List<ResourceEntity> list_all = new LinkedList<>();
+            List<ResourceEntity> list = new LinkedList<>();
             ResourceDaoImpl resourceDao = new ResourceDaoImpl();
-            list = resourceDao.getAllResourceList();
+            list_all = resourceDao.getAllResourceList();
             while(rs.next()) {
-                list.forEach(resources -> {
+                list_all.forEach(resources -> {
                     try {
                         if(resources.getId().equals(rs.getInt("b.resource_id"))) {
-                            map.put(new BuildingsEntity(){{
-                                setId(rs.getInt("a.id"));
-                                setName(rs.getString("a.name"));
-                                setDescription(rs.getString("a.description"));
-                                setNumber_per_sec(rs.getFloat("b.number_per_sec"));
-                            }},resources);
+                            list.add(resources);
                         }
                     } catch (SQLException e) {
                         System.out.println(e);
                     }
                 });
+                map.put(new BuildingEntity(){{
+                    setId(rs.getInt("a.id"));
+                    setName(rs.getString("a.name"));
+                    setDescription(rs.getString("a.description"));
+                    setNumber_per_sec(rs.getFloat("b.number_per_sec"));
+                }}, list);
+                list.clear();
             }
             return map;
         });
